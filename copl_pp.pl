d5fac09afd8743523c1c843ab5f46b7b8e8d50f0
Masaki Hara <ackie.h.gmai@gmail.com>
2020-01-02 18:16:10 +0900

    pp_prop, pp_nat

--- copl_pp.pl
+++ copl_pp.pl
@@ -1,6 +1,6 @@
 % vim: ft=prolog :
 
-:- module(copl_pp, [pp_proof/2]).
+:- module(copl_pp, [pp_proof/2, pp_prop/2, pp_nat/2]).
 
 pp_proof(Proof, S) :-
   pp_proof(Proof, "", [], Parts),
@@ -34,3 +34,28 @@ pp_proof1c([ArgH|ArgT], ";", I, Rem, [I|Parts]) :-
   !,
   pp_proof(ArgH, I, [Sep, "\n"|Rem1], Parts),
   pp_proof1c(ArgT, Sep, I, Rem, Rem1).
+
+pp_prop(plus_is(N1, N2, N3), S) :-
+  !,
+  pp_nat(N1, N1S),
+  pp_nat(N2, N2S),
+  pp_nat(N3, N3S),
+  atomics_to_string([N1S, " plus ", N2S, " is ", N3S], S).
+pp_prop(times_is(N1, N2, N3), S) :-
+  !,
+  pp_nat(N1, N1S),
+  pp_nat(N2, N2S),
+  pp_nat(N3, N3S),
+  atomics_to_string([N1S, " times ", N2S, " is ", N3S], S).
+pp_prop(Prop, S) :-
+  term_string(Prop, PropS),
+  atomics_to_string(["<cannot pp ", PropS, ">"], S).
+
+pp_nat(z, "Z") :- !.
+pp_nat(s(N), S) :-
+  !,
+  pp_nat(N, NS),
+  atomics_to_string(["S(", NS, ")"], S).
+pp_nat(N, S) :-
+  term_string(N, NS),
+  atomics_to_string(["<cannot pp ", NS, ">"], S).
--- pp_test.pl
+++ pp_test.pl
@@ -2,7 +2,7 @@
 
 :- use_module([copl_pp]).
 
-:- begin_tests(pp).
+:- begin_tests(pp_proof).
   test(test_pp1) :-
     pp_proof(pf(plus_is(z, z, z), 'P-Zero'), S),
     assertion(S == "plus_is(z,z,z) by P-Zero {}").
@@ -14,7 +14,34 @@
       "}"
     ], Expect),
     assertion(S == Expect).
-:- end_tests(pp).
+:- end_tests(pp_proof).
+
+:- begin_tests(pp_prop).
+  test(pp_plus_is_0) :-
+    pp_prop(plus_is(z, z, z), S),
+    assertion(S == "Z plus Z is Z").
+  test(pp_plus_is_1) :-
+    pp_prop(plus_is(s(z), s(s(z)), s(s(s(z)))), S),
+    assertion(S == "S(Z) plus S(S(Z)) is S(S(S(Z)))").
+  test(pp_times_is_0) :-
+    pp_prop(times_is(z, z, z), S),
+    assertion(S == "Z times Z is Z").
+  test(pp_times_is_1) :-
+    pp_prop(times_is(s(s(z)), s(s(z)), s(s(s(s(z))))), S),
+    assertion(S == "S(S(Z)) times S(S(Z)) is S(S(S(S(Z))))").
+:- end_tests(pp_prop).
+
+:- begin_tests(pp_nat).
+  test(pp_nat_0) :-
+    pp_nat(z, S),
+    assertion(S == "Z").
+  test(pp_nat_1) :-
+    pp_nat(s(z), S),
+    assertion(S == "S(Z)").
+  test(pp_nat_2) :-
+    pp_nat(s(s(z)), S),
+    assertion(S == "S(S(Z))").
+:- end_tests(pp_nat).
 
 :- run_tests.
 :- halt.
